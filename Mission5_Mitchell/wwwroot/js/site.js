3e8156ae68d9f12438faaf475ddfa2c780744711
allie <allie@ALEXANDRAMI8C4F>
2024-02-08 20:30:30 -0700

    Add project files.

---
+++ Mission5_Mitchell/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿$("#btnsubmit").click(function () {
+    var hours = $("#hours").val();
+    var amount = $("#rate").val();
+    var calculatedTotal = $("#total");
+
+    calculatedTotal.prop('readonly', false);
+
+    calculatedTotal.val(hours * amount);
+
+    calculatedTotal.prop('readonly', true);
+});
